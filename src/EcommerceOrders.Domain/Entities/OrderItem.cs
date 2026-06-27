cc5e83641558ac141d216784c3c5a66baaf7a033
Arthur  Matheus dos Santos Lima <arthur.lima@delbank.com.br>
2026-06-27 16:21:04 -0300

    refactor(domain): introduce item quantity and calculate automatic order total value

--- src/EcommerceOrders.Domain/Entities/OrderItem.cs
+++ src/EcommerceOrders.Domain/Entities/OrderItem.cs
@@ -5,10 +5,13 @@ public class OrderItem
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public decimal Price { get; private set; }
+    public int Quantity { get; private set; }
+    
+    public decimal TotalValue => Price * Quantity;
     
     private OrderItem() {}
 
-    public OrderItem(string name, decimal price)
+    public OrderItem(string name, decimal price, int quantity)
     {
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Product name cannot be empty.");
@@ -16,8 +19,12 @@ public class OrderItem
         if (price <= 0)
             throw new ArgumentException("Price must be greater than zero.");
         
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity must be greater than zero.");
+        
         Id = Guid.NewGuid();
         Name = name;
         Price = price;
+        Quantity = quantity;
     }
 }
